ecdf6948a7664eeea2a81cc87d8014e15b693ab1
Vlad Shubin <shubin.va@phystech.edu>
2023-03-29 00:26:25 +0300

    fix some bugs on 2nd strategy

--- src/models/baselines/__init__.py
+++ src/models/baselines/__init__.py
@@ -80,7 +80,7 @@ class MulticlassClassifier:
     @staticmethod
     def take_subsample(classes, data):
         data = data[(data.iloc[:, -1:] == classes[0]) | (data.iloc[:, -1:] == classes[1])]
-        target = pd.Series(data.iloc[:, -1:]).replace([classes], [0, 1])
+        target = data.iloc[:, -1:].apply(lambda x: x == classes[1]).astype(int)
         return pd.concat([data.iloc[:, :-1], target], axis=1)
 
     def make_array_of_classifiers(self, n):
@@ -100,11 +100,14 @@ class MulticlassClassifier:
                 self.classifiers[i].fit(X_train, y_train)
 
         elif self.mode == self.strategies[1]:
-            self.make_array_of_classifiers((len(self.classes) * (len(self.classes) + 1)) // 2)
+            num_of_classifiers = (len(self.classes) * (len(self.classes) + 1)) // 2
+            self.make_array_of_classifiers(num_of_classifiers)
 
             cur_cls = 0
+            self.sub_samples = [[None] * 2] * num_of_classifiers
             for i in range(len(self.classes)):
                 for j in range(i + 1, len(self.classes)):
+                    self.sub_samples[cur_cls][0], self.sub_samples[cur_cls][1] = i, j
                     data = MulticlassClassifier.take_subsample((i, j), train_data)
                     X_train, y_train = data.iloc[:, :-1], data.iloc[:, -1:]
                     self.classifiers[cur_cls].fit(X_train, y_train)
@@ -137,11 +140,9 @@ class MulticlassClassifier:
             predictions = pd.DataFrame({'pred': [None] * len(X)})
 
             for k in range(len(self.classifiers)):
-                proba = self.classifiers[k].predict(X)
-                pred = pd.Series(np.where(proba < threshold,
-                                          self.sub_samples[k][0],
-                                          self.sub_samples[k][1]))
-                predictions = pd.concat([predictions, pred], axis=1)
+                proba = pd.DataFrame(self.classifiers[k].predict(X))
+                #pred = proba.apply(lambda x: self.sub_samples[k][0] if x < threshold else self.sub_samples[k][1]) TODO
+                #predictions = pd.concat([predictions, pd.DataFrame(pred)], axis=1)
 
             predictions.drop(columns=['pred'], inplace=True)
 
@@ -150,11 +151,11 @@ class MulticlassClassifier:
         return y_pred
 
     def _voting_of_classifiers(self, predictions, y_pred):
-        for i, row in predictions.iterrows():
+        for i, y in predictions.iterrows():
             classes = [0] * len(self.classes)
-            lead_cls = 0
-            for j in range(len(row)):
-                classes[row[j]] += 1
-                if classes[row[j]] > classes[lead_cls]:
-                    lead_cls = row[j]
-            y_pred[i] = lead_cls
+            lead_y = 0
+            for y_i in y:
+                classes[y_i] += 1
+                if classes[y_i] > classes[lead_y]:
+                    lead_y = y_i
+            y_pred[i] = lead_y
